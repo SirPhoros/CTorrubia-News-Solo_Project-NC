b240958c527eb979ed8b3be595001de3ce67d4a9
SirPhoros <cgtorrubia@gmail.com>
2023-05-09 13:37:58 +0100

    3. GET api/topics - error handling

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const { getTopics } = require('./controllers/topics.controllers')
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-
 //TOPICS
 app.get('/api/topics', getTopics)
+
+//ERROR HANDLING
+app.use((err, req, res, next) => {
+	if (err.status && err.msg) {
+		res.status(err.status).send({ msg: err.msg })
+	} else {
+		next(err)
+	}
+})
+
+app.use((err, req, res, next) => {
+	res.status(404).send({ msg: 'endpoint not found' })
+
+	next(err)
+})
+
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).send({ msg: "server error! We're very sorry" })
+})
 
 module.exports = app
