6f12942983575d924b81e70ca2e6eafd2010f4bf
SirPhoros <cgtorrubia@gmail.com>
2023-05-09 14:03:12 +0100

    GET /api - Error Handling

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -3,7 +3,6 @@ const { fetchApi } = require('../models/api.models')
 exports.getAPI = (req, res, next) => {
 	fetchApi()
 		.then((endpoints) => {
-			console.log(endpoints, "endpoints")
 			res.status(200).send({ endpoints: endpoints })
 		})
 		.catch((err) => {
