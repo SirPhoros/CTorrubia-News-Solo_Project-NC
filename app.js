2886904bb9e7a830deaccb0c0f555e508419d5b1
SirPhoros <cgtorrubia@gmail.com>
2023-05-09 15:53:51 +0100

    make changes to tests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { getAPI } = require('./controllers/api.controller')
+const { getArticleId } = require('./controllers/articles.controller')
 const { getTopics } = require('./controllers/topics.controllers')
 
 const express = require('express')
@@ -12,9 +13,28 @@ app.get('/api/topics', getTopics)
 //API
 app.get('/api', getAPI)
 
+//ARTICLES
+app.get('/api/articles/:article_id', getArticleId)
+
 //ERROR HANDLING
 app.all('*', (req, res) => {
 	res.status(404).send({ msg: 'endpoint not found' })
+})
+
+app.use((err, req, res, next) => {
+	if (err.code === '22P02') {
+		res.status(400).send({ msg: 'Bad request: Not valid type of input' })
+	} else {
+		next(err)
+	}
+})
+
+app.use((err, req, res, next) => {
+	if (err.status && err.msg) {
+		res.status(err.status).send({ msg: err.msg })
+	} else {
+		next(err)
+	}
 })
 
 app.use((err, req, res, next) => {
